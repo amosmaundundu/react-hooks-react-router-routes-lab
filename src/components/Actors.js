c3096504a007607b392c80a2766b9c90c369bd50
amosmaundundu <amosmaundu5@gmail.com>
2022-09-30 15:17:20 +0300

    src

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,18 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Actors</h1>
+    {actors.map((actor)=>(
+      <div key={actors.title}>
+        <h2>{actor.title}</h2>
+        <ul>
+          {actor.movies.map((movie)=>(
+          <li key={movie}>{movie}</li>))}
+        </ul>
+      </div>
+    ))}
+  </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,18 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+   <h1>Directors</h1> 
+   {directors.map((Director)=>(
+      <div key={Director.title}>
+        <h2>{Director.title}</h2>
+        <ul>
+          {Director.movies.map((movie)=>(
+          <li key={movie}>{movie}</li>))}
+        </ul>
+      </div>
+      ))}
+  </div>;
 }
 
 export default Directors;
